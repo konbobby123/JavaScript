b6be7be6bc2694c826da0203566c84f7c4e228a5
konbobby123 <163637582+konbobby123@users.noreply.github.com>
2024-10-01 20:48:36 +0530

    upadte

---
+++ Day 1/index.js
@@ -0,0 +1,59 @@
+// 1 .Datatype
+// String
+// Number
+// Boolean
+// undefined
+// 1.1 string
+//"bobby"
+//'konbooby'
+//`123`
+console.log("konbobby, bhushan")
+console.log(typeof("Pawar"))
+console.log(typeof("123"))
+console.log(typeof(`xyz`))
+
+//number
+// 123
+// 1234
+// @#kfsd
+console.log(typeof(a))
+console.log(typeof(12))
+console.log(typeof(4.5))
+
+// boolean
+console.log(typeof('true'))
+console.log(typeof('false'))
+
+// Assign =
+// ressign =
+// delclare =
+// var - store Data 
+
+var myname= "buddhabhushan"
+console.log(myname,"myname")
+
+var myage= " 20"
+console.log(myage,"myage")
+
+var myage='22'
+console.log( myage)
+
+
+var number=20; // 
+number=40;
+console.log(number)
+
+var number1=100;
+var number2=100;
+console.log(number1+number2)
+console.log( number1-number2)
+console.log(number1*number2)
+console.log( number1/number2)
+ 
+var firstname= "hello"
+var secondname="hello"
+// console.log( first== second)
+console.log( "1"== "1")
+console.log( "2"=="3" )
+
+
